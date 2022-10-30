f0cf5a6d96d941fef02d1c9fc4068f95a8259c36
Nkaka23dev <nkakaeric96@gmail.com>
2022-10-30 06:39:47 +0200

    pushing on dev

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
    <Fragment>
     <div className="flex h-screen">
-    <h1 className="text-3xl font-bold text-green-600  m-auto">
-      Coming soon........
+    <h1 className="text-3xl font-bold text-green-400 lowercase  m-auto">
+      cONTENT TO BE LOADED SOON......
     </h1>
     </div>
    </Fragment>
